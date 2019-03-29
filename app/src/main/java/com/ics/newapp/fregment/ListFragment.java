3feed851e9037d53f0d49fd4c3208846fe9a8a8c
Raghav <raghvendra.19934@gmail.com>
2019-03-29 11:43:55 +0530

    new file

--- app/src/main/java/com/ics/newapp/fregment/ListFragment.java
+++ app/src/main/java/com/ics/newapp/fregment/ListFragment.java
@@ -1,18 +1,28 @@
 package com.ics.newapp.fregment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ics.newapp.R;
 
 
 public class ListFragment extends Fragment {
+    LinearLayout fav_list,add_event,view_profile;
+    CardView btn_M_view,btn_L_view;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,5 +36,51 @@ public class ListFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
+
+        fav_list=(LinearLayout)view.findViewById(R.id.fav_list);
+        add_event=(LinearLayout)view.findViewById(R.id.add_event);
+        view_profile=(LinearLayout)view.findViewById(R.id.provile_view);
+        btn_L_view=(CardView)view.findViewById(R.id.m_view);
+        btn_M_view=(CardView)view.findViewById(R.id.l_view);
+
+        fav_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment Favorite_List=new Favorite_list();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_frame,Favorite_List);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        view_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment profile_List=new profile_fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_frame,profile_List);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        btn_M_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment mapview=new MapFregment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_frame,mapview);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+
+
     }
+
 }
