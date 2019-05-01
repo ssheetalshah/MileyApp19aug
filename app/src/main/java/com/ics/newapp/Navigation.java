3312d40858f9730204d59297bbd476cdcd7d0315
Raghvendra Sahu <raghvendra.19934@gmaial.com>
2019-05-01 18:41:02 +0530

    by rrrr

--- app/src/main/java/com/ics/newapp/Navigation.java
+++ app/src/main/java/com/ics/newapp/Navigation.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.ics.newapp.fregment.Create_Activity;
 import com.ics.newapp.fregment.Event_Host_User_Profile;
@@ -29,6 +31,8 @@ import com.ics.newapp.fregment.profile_fragment;
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    LinearLayout ll_mlist,ll_create_act;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,27 @@ public class Navigation extends AppCompatActivity
         tx.replace(R.id.content_frame, new MapFregment());
         tx.commit();
 
+
+        ll_mlist=findViewById(R.id.mlist);
+        ll_create_act=findViewById(R.id.create_act);
+
+        ll_mlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, new ListFragment());
+                ft.commit();
+            }
+        });
+
+        ll_create_act.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, new Create_Activity());
+                ft.commit();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
